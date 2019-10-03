3bbe0e6b7cb1dd841ad2ec5147a45773b6d35ea0
Adam Hartleb <adamjhartleb@gmail.com>
2019-10-02 19:02:35 -0700

    Improved caesar.py a bit

--- caesar.py
+++ caesar.py
@@ -6,20 +6,27 @@ def encode(word, key):
     letters = list(word)
     encrypted_letters = []
     for letter in letters:
-        if not(letter in ascii_letters):
-            raise ValueError("Word must consist of only letters.")
-
-        isUppercase = letter.isupper()
-        letter = letter.lower()
         encrypted_letter = ''
-        if ord(letter) + key > ord('z'):
-            encrypted_letter = chr(ord(letter) - 26 + key)
+        if letter == ' ':
+            encrypted_letter = ' '
+        elif not(letter in ascii_letters):
+            raise ValueError("Word must consist of only letters.")
         else:
-            encrypted_letter = chr(ord(letter) + key)
-        encrypted_letter = encrypted_letter.upper() if isUppercase else encrypted_letter
+            encrypted_letter = shift_letter(letter, key)
         encrypted_letters.append(encrypted_letter)
 
     return ''.join(encrypted_letters)
+
+
+def shift_letter(letter, key):
+    isUppercase = letter.isupper()
+    letter = letter.lower()
+    encrypted_letter = ''
+    if ord(letter) + key > ord('z'):
+        encrypted_letter = chr(ord(letter) - 26 + key)
+    else:
+        encrypted_letter = chr(ord(letter) + key)
+    return encrypted_letter.upper() if isUppercase else encrypted_letter
 
 
 def main():
