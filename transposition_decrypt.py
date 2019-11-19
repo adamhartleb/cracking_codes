d4e5f7b4367b34f4d01bb2ca0a143563bd28324a
Adam Hartleb <adamjhartleb@gmail.com>
2019-11-19 15:08:18 -0800

    Finished transposition decrypter

--- transposition_decrypt.py
+++ transposition_decrypt.py
@@ -1,37 +1,46 @@
 from math import ceil
+from sys import argv
 
 
 def transposition_decrypt(encrypted_message, key):
-    num_columns = ceil(len(encrypted_message) / key)
-    null_boxes = num_columns * key - len(encrypted_message)
-    null_row_start = key - null_boxes
+    num_matrix_columns = ceil(len(encrypted_message) / key)
+    num_matrix_rows = key
+    num_matrix_elements = num_matrix_columns * num_matrix_rows
+    num_null_boxes = num_matrix_columns * key - len(encrypted_message)
+    null_row_start = num_matrix_rows - num_null_boxes
     matrix = [[] for _ in range(key)]
-    index = 0
-    letterIndex = 0
 
-    while index < num_columns * key:
-        print(encrypted_message[letterIndex])
-        letter = encrypted_message[letterIndex]
+    matrix_index = 0
+    letter_index = 0
+    while matrix_index < num_matrix_elements:
+        row_number = matrix_index // num_matrix_columns
 
-        row_number = index // num_columns
-        if row_number >= null_row_start and ((index + 1) % num_columns) == 0:
+        is_start_of_null_row = row_number >= null_row_start
+        is_last_element_in_row = (matrix_index + 1) % num_matrix_columns == 0
+
+        if is_start_of_null_row and is_last_element_in_row:
             matrix[row_number].append(None)
         else:
-            matrix[index // num_columns].append(letter)
-            letterIndex += 1
-        index += 1
-    print(matrix)
-    unecrypted_string = ''
-    for column_number in range(num_columns):
-        for row in matrix:
-            letter = row[column_number]
+            letter = encrypted_message[letter_index]
+            matrix[row_number].append(letter)
+            letter_index += 1
+        matrix_index += 1
+
+    decrypted_message = ''
+    for column_number in range(num_matrix_columns):
+        for row_number in range(num_matrix_rows):
+            letter = matrix[row_number][column_number]
             if letter:
-                unecrypted_string += letter
-    print(unecrypted_string)
+                decrypted_message += letter
+    return decrypted_message
 
 
 def main():
-    transposition_decrypt('Cenoonommstmme oo snnio. s s c', 8)
+    message, key = argv[1:]
+
+    decrypted_message = transposition_decrypt(message, int(key))
+
+    print(decrypted_message)
 
 
 if __name__ == "__main__":
